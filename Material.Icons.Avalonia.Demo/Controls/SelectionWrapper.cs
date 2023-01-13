fde36edbbb606798c7ee1ee9da10bbb7de19eee1
SKProCH <skproch@yandex.ru>
2023-01-14 02:35:17 +0300

    Updates and fixes in Avalonia demo project

--- Material.Icons.Avalonia.Demo/Controls/SelectionWrapper.cs
+++ Material.Icons.Avalonia.Demo/Controls/SelectionWrapper.cs
@@ -5,7 +5,16 @@ using Avalonia.Controls.Metadata;
 
 namespace Material.Icons.Avalonia.Demo.Controls {
     [PseudoClasses("selectednow")]
-    public class SelectionWrapper : UserControl {
+    public class SelectionWrapper : ContentControl {
+        public static readonly StyledProperty<object> DataSourceProperty = AvaloniaProperty.Register<SelectionWrapper, object>(nameof(DataSource));
+
+        public static readonly StyledProperty<object> CurrentSelectedProperty = AvaloniaProperty.Register<SelectionWrapper, object>(nameof(CurrentSelected));
+
+        public static readonly DirectProperty<SelectionWrapper, bool> SelectedNowProperty = AvaloniaProperty.RegisterDirect<SelectionWrapper, bool>(
+            nameof(SelectedNow),
+            wrapper => wrapper.CurrentSelected == wrapper.DataSource);
+
+        private bool _selectedNow;
         static SelectionWrapper() {
             PointerPressedEvent.Raised.Subscribe(tuple => {
                 if (tuple.Item1 is SelectionWrapper selectionWrapper) {
@@ -17,6 +26,10 @@ namespace Material.Icons.Avalonia.Demo.Controls {
                 if (args.Sender is SelectionWrapper selectionWrapper) {
                     selectionWrapper.UpdateSelectedNow();
                 }
+            });
+
+            DataSourceProperty.Changed.Subscribe(args => {
+                if (args.Sender is SelectionWrapper selectionWrapper) selectionWrapper.UpdateSelectedNow();
             });
 
             SelectedNowProperty.Changed.Subscribe(args => {
@@ -31,38 +44,23 @@ namespace Material.Icons.Avalonia.Demo.Controls {
             });
         }
 
-        protected override void OnDataContextEndUpdate() {
-            base.OnDataContextEndUpdate();
-            UpdateSelectedNow();
-        }
-
-        public void UpdateSelectedNow() {
-            SelectedNow = DataSource != null && DataSource == CurrentSelected;
-        }
-
-        public static readonly StyledProperty<object> DataSourceProperty = AvaloniaProperty.Register<SelectionWrapper, object>(nameof(DataSource));
-
         public object DataSource {
             get => GetValue(DataSourceProperty);
             set => SetValue(DataSourceProperty, value);
         }
-
-        public static readonly StyledProperty<object> CurrentSelectedProperty = AvaloniaProperty.Register<SelectionWrapper, object>(nameof(CurrentSelected));
 
         public object CurrentSelected {
             get => GetValue(CurrentSelectedProperty);
             set => SetValue(CurrentSelectedProperty, value);
         }
 
-        public static readonly DirectProperty<SelectionWrapper, bool> SelectedNowProperty = AvaloniaProperty.RegisterDirect<SelectionWrapper, bool>(
-            nameof(SelectedNow),
-            wrapper => wrapper.CurrentSelected == wrapper.DataSource);
-
-        private bool _selectedNow;
-
         public bool SelectedNow {
             get => _selectedNow;
             private set => SetAndRaise(SelectedNowProperty, ref _selectedNow, value);
+        }
+
+        public void UpdateSelectedNow() {
+            SelectedNow = DataSource != null && DataSource == CurrentSelected;
         }
     }
 }
--- Material.Icons.Avalonia.Demo/Views/MainWindow.axaml.cs
+++ Material.Icons.Avalonia.Demo/Views/MainWindow.axaml.cs
@@ -1,4 +1,3 @@
-using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Diagnostics;
 using Avalonia.Input;
