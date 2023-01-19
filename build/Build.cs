f15f67ad9032fb1f75e2022d5a7745b17c1eb1f4
SKProCH <skproch@yandex.ru>
2023-01-20 02:26:29 +0300

    Add proper bump version in nuke

--- build/Build.cs
+++ build/Build.cs
@@ -1,23 +1,28 @@
+using System;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Text;
 using Generators;
 using Generators.PathDataGenerators;
 using Meta;
+using Microsoft.Build.Evaluation;
 using Nuke.Common;
 using Nuke.Common.CI;
 using Nuke.Common.Execution;
 using Nuke.Common.Git;
 using Nuke.Common.ProjectModel;
 using Nuke.Common.Tools.DotNet;
+using Nuke.Common.Tools.Git;
 using Serilog;
 using static Nuke.Common.Tools.DotNet.DotNetTasks;
+// ReSharper disable InconsistentNaming
 
 [ShutdownDotNetAfterServerBuild]
 [SuppressMessage("ReSharper", "UnusedMember.Local")]
-class Build : NukeBuild {
+partial class Build : NukeBuild {
     /// Support plugins are available for:
     ///   - JetBrains ReSharper        https://nuke.build/resharper
     ///   - JetBrains Rider            https://nuke.build/rider
@@ -28,44 +33,73 @@ class Build : NukeBuild {
     [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
     readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
 
+    [Parameter("Should we skip compilation of WPF projects. In non windows systems this is enabled by default")]
+    readonly bool SkipWpfCompilation = false;
+
     [Solution] readonly Solution Solution;
     [GitRepository] readonly GitRepository GitRepository;
 
-    Target Clean => _ => _
-        .Before(Restore)
-        .Executes(() =>
-            { });
-
     Target Restore => _ => _
-        .Executes(() =>
-        {
+        .Executes(() => {
             DotNetRestore(s => s
                 .SetProjectFile(Solution));
         });
 
     Target Compile => _ => _
         .DependsOn(Restore)
-        .Executes(() =>
-        {
-            DotNetBuild(s => s
-                .SetProjectFile(Solution)
-                .SetConfiguration(Configuration)
-                .EnableNoRestore());
+        .Executes(() => {
+            if (Environment.OSVersion.Platform == PlatformID.Win32NT && !SkipWpfCompilation) {
+                Log.Information("Due to running on Windows and SkipWpfCompilation=False, we will compile the whole solution");
+                ExecuteBuildFor(Solution);
+            }
+            else {
+                ExecuteBuildFor("Material.Icons");
+                ExecuteBuildFor("Material.Icons.Avalonia");
+                ExecuteBuildFor("Material.Icons.Avalonia.Demo");
+            }
+            
+            void ExecuteBuildFor(string projectFile) {
+                DotNetBuild(s => s
+                    .SetProjectFile(projectFile)
+                    .SetConfiguration(Configuration)
+                    .EnableNoRestore());
+            }
         });
 
     Target UpdateIcons => _ => _
-        .Executes(async () =>
-        {
+        .Executes(async () => {
             Log.Information("Downloading icons meta information");
             var iconsEnumerable = await MaterialIconsMetaTools.GetIcons();
             var iconInfos = iconsEnumerable.ToList();
 
             var destinationPath = Solution.Directory / "Material.Icons";
             Log.Information("Writing icons meta information to {DestinationPath}", destinationPath);
-            
+
             MaterialIconKindEnumGenerator.Write(destinationPath, iconInfos);
             MaterialIconDataDeclarationGenerator.Write(destinationPath);
             Switch8uStringsGenerator.Write(destinationPath, iconInfos);
         })
         .Triggers(Compile);
+
+    Target GhUpdateIconsWorkflow => _ => _
+        .DependsOn(UpdateIcons, GhBumpVersionIfNeeded);
+
+    Target GhBumpVersionIfNeeded => _ => _
+        .OnlyWhenDynamic(() => !GitTasks.GitHasCleanWorkingCopy())
+        .DependsOn(Compile)
+        .Executes(() => {
+            var project = Solution.GetProject("Material.Icons").GetMSBuildProject();
+
+            var version = Version.Parse(project.GetProperty("Version").EvaluatedValue);
+            var newVersion = new Version(version.Major, version.Minor, version.Build + 1);
+            project.SetProperty("Version", newVersion.ToString());
+
+            var currentTime = DateTime.UtcNow.ToString("R", CultureInfo.InvariantCulture);
+            var newReleaseNotes = $"- Icons set updated according to materialdesignicons.com at {currentTime}{Environment.NewLine}"
+                                + "Check out changes at https://pictogrammers.com/library/mdi/history/";
+            project.SetProperty("PackageReleaseNotes", newReleaseNotes);
+
+            project.Save();
+            Log.Information("Bumped Material.Icon property to {NewVersion}", newVersion);
+        });
 }
