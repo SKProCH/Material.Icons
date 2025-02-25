3fdfc94791327bebc6ae58837de37c05629ddee7
Neakita <thekatterdev@gmail.com>
2025-02-25 14:00:38 +0500

    Change MaterialIconTextExt to return MaterialIconText control instead of making layout in place
    (cherry picked from commit 6aa403b859d4a72bf2710f4a6e56a880338090e9)

--- Material.Icons.Avalonia/MaterialIconTextExt.cs
+++ Material.Icons.Avalonia/MaterialIconTextExt.cs
@@ -12,47 +12,34 @@ namespace Material.Icons.Avalonia
         public MaterialIconTextExt(MaterialIconKind kind, double? size) : base(kind, size) { }
 
         [ConstructorArgument("spacing")]
-        public double Spacing { get; set; } = 5;
+        public double? Spacing { get; set; } = 5;
 
         [ConstructorArgument("orientation")]
-        public Orientation Orientation { get; set; } = Orientation.Horizontal;
+        public Orientation? Orientation { get; set; } = global::Avalonia.Layout.Orientation.Horizontal;
 
         [ConstructorArgument("text")]
         public string? Text { get; set; }
 
         [ConstructorArgument("textFirst")]
-        public bool TextFirst { get; set; } = false;
+        public bool? TextFirst { get; set; } = false;
 
         [ConstructorArgument("isTextSelectable")]
-        public bool IsTextSelectable { get; set; } = false;
+        public bool? IsTextSelectable { get; set; } = false;
         
         public override object ProvideValue(IServiceProvider serviceProvider) {
-            var icon = (Control)base.ProvideValue(serviceProvider);
-
-            if (string.IsNullOrWhiteSpace(Text)) return icon;
-
-            var textBlock = IsTextSelectable ?
-                new SelectableTextBlock {
-                    Text = Text,
-                    HorizontalAlignment = HorizontalAlignment.Center,
-                    VerticalAlignment = VerticalAlignment.Center,
-                }
-                : new TextBlock {
-                    Text = Text,
-                    HorizontalAlignment = HorizontalAlignment.Center,
-                    VerticalAlignment = VerticalAlignment.Center,
-                };
-
-            if (Size.HasValue) textBlock.FontSize = Size.Value;
-
-            return new StackPanel {
-                Orientation = Orientation,
-                Spacing = Spacing,
-                Children = {
-                    TextFirst ? textBlock : icon,
-                    TextFirst ? icon : textBlock
-                }
-            };
+            if (string.IsNullOrWhiteSpace(Text)) return (Control)base.ProvideValue(serviceProvider);
+            var result = new MaterialIconText();
+            if (Spacing.HasValue) result.Spacing = Spacing.Value;
+            if (Orientation.HasValue) result.Orientation = Orientation.Value;
+            if (TextFirst.HasValue) result.TextFirst = TextFirst.Value;
+            if (IsTextSelectable.HasValue) result.IsTextSelectable = IsTextSelectable.Value;
+            if (Size.HasValue) {
+                result.IconSize = Size.Value;
+                result.FontSize = Size.Value;
+            }
+            result.Kind = Kind;
+            result.Text = Text;
+            return result;
         }
     }
 }
