50d5e4862bdf96457b75bdd0fae69a05740a3ab6
Neakita <thekatterdev@gmail.com>
2025-02-27 12:56:24 +0500

    Add docs to Avalonia MaterialIconText properties accessors

--- Material.Icons.Avalonia/MaterialIconText.axaml.cs
+++ Material.Icons.Avalonia/MaterialIconText.axaml.cs
@@ -22,31 +22,49 @@ namespace Material.Icons.Avalonia {
         public static readonly StyledProperty<double> IconSizeProperty =
             AvaloniaProperty.Register<MaterialIconText, double>(nameof(IconSize), defaultValue: double.NaN);
 
+        /// <summary>
+        /// Gets or sets the spacing between the icon and the text.
+        /// </summary>
         public double Spacing {
             get => GetValue(SpacingProperty);
             set => SetValue(SpacingProperty, value);
         }
 
+        /// <summary>
+        /// Gets or sets the orientation in which the icon and the text will be layed out.
+        /// </summary>
         public Orientation Orientation {
             get => GetValue(OrientationProperty);
             set => SetValue(OrientationProperty, value);
         }
 
+        /// <summary>
+        /// Gets or sets the text to display
+        /// </summary>
         public string? Text {
             get => GetValue(TextProperty);
             set => SetValue(TextProperty, value);
         }
 
+        /// <summary>
+        /// Gets or sets whether the text should appear on the left side instead of the right
+        /// </summary>
         public bool TextFirst {
             get => GetValue(TextFirstProperty);
             set => SetValue(TextFirstProperty, value);
         }
 
+        /// <summary>
+        /// Gets or sets whether the text should be selectable
+        /// </summary>
         public bool IsTextSelectable {
             get => GetValue(IsTextSelectableProperty);
             set => SetValue(IsTextSelectableProperty, value);
         }
 
+        /// <summary>
+        /// Gets or sets the width and the height of the icon
+        /// </summary>
         public double IconSize {
             get => GetValue(IconSizeProperty);
             set => SetValue(IconSizeProperty, value);
