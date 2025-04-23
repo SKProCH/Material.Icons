c405fc1c721d07c5c19c18f9f6de81d22007fa09
SKProCH <skproch@yandex.ru>
2025-04-24 02:03:57 +0300

    Tries to unfuck winui project one more time

---
+++ Material.Icons.WinUI3/ComparerDoubleTrigger.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.UI.Xaml;
+
+namespace Material.Icons.WinUI3;
+
+internal class ComparerDoubleTrigger : StateTriggerBase {
+    public static readonly DependencyProperty ValueProperty = DependencyProperty.Register(
+        nameof(Value), typeof(object), typeof(ComparerDoubleTrigger), 
+        new PropertyMetadata(null, OnValueChanged));
+
+    private static void OnValueChanged(DependencyObject d, DependencyPropertyChangedEventArgs e) {
+        var trigger = (ComparerDoubleTrigger)d;
+        trigger.UpdateTrigger();
+    }
+
+    public object? Value {
+        get { return (object?)GetValue(ValueProperty); }
+        set { SetValue(ValueProperty, value); }
+    }
+    
+    public bool IsEqual { get; set; }
+    
+    private void UpdateTrigger() {
+        SetActive(double.IsNaN(Value as double? ?? 0));
+    }
+}
