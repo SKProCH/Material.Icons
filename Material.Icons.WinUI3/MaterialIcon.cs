e35735199a2bc59287b08c97ec118bbba5e855c4
SKProCH <skproch@yandex.ru>
2026-02-07 15:04:33 +0300

    Handle null Kind in UpdateData method in winui

--- Material.Icons.WinUI3/MaterialIcon.cs
+++ Material.Icons.WinUI3/MaterialIcon.cs
@@ -73,6 +73,6 @@ public partial class MaterialIcon : Control {
         => ((MaterialIcon)dependencyObject).UpdateData();
 
     private void UpdateData() {
-        Data = MaterialIconDataProvider.Get<Geometry>(Kind);
+        Data = Kind is null ? null : MaterialIconDataProvider.Get<Geometry>(Kind.Value);
     }
 }
